b54d1975b347fc877158731fb26a9a6168f91175
Brandon Hornick <158857799+bhornick09@users.noreply.github.com>
2026-05-06 16:56:57 -0400

    add youtube download functionality

--- app/worker.py
+++ app/worker.py
@@ -1,5 +1,14 @@
-import time
+from pytube import YouTube
+
 def process_job(job_id, jobs):
-    jobs[job_id]["status"] = "processing"
-    time.sleep(5)  # Simulate time-consuming task
-    jobs[job_id]["status"] = "completed"
+    jobs[job_id]["status"] = "downloading"
+    url = jobs[job_id]["url"]
+
+    try:
+        yt= Youtube(url)
+        stream = yt.streams.get_highest_resolution()
+        stream.download(output_path="downloads/")
+        jobs[job_id]["status"] = "completed"
+    except Exception as e:
+        jobs[job_id]["status"] = f"error: {str(e)}"
+    
